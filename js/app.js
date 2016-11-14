282e041a13d2bdc5b9fdc4cf5d1d6775d1efce43
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-11-14 11:41:03 -0500

    cambios en geolocalizacion

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $(".load").fadeOut(3000);
 
+    $("#btnext3").attr("disabled");
+
     $("#formulario").validate({
     	rules:{
     		celular:{
@@ -41,14 +43,26 @@ $(document).ready(function() {
         $("#formulario").submit();
             window.location.href = "register.html";
     });
-    
-    $("#nombre").validate({
+    $("#form").validate({
         rules:{
             nombre: {
                 required: true,
                 digits: false,
                 minlength: 2
             },
+            apellido: {
+                required: true,
+                digits: false,
+                minlength: 2
+              },
+            email: {
+                required: true,
+                digits: false,
+                minlength: 2
+            },
+            box:{
+                required:true,
+            }
         },
         messages:{
             nombre: {
@@ -56,48 +70,41 @@ $(document).ready(function() {
                 digits: "Ingresa tu nombre",
                 minlength: "Escriba su nombre"
               },
+            apellido: {
+                required: "Campo Obligatorio",
+                digits: "Ingresa tu Apellido",
+                minlength: "Escriba su apellido"
+              },
+            email: {
+                required: "El campo es requerido",
+                digits: "Ingresa tu E-mail",
+                minlength: "Escriba su Email",
+            },
+            box:{
+                required:"Campo es requerido"
+            }
         },
-    })
+        submitHandler: function() {
+            $("#btnext3").removeAttr("disabled");
+        }
+    });    
+
 
     $("#btnext3").on("click", function(){
-        $("#formulario").submit();
-                window.location.href = "mapa.html";
+       if($("#form").valid()){
+            window.location.href = "mapa.html";
+       }
     });
 
+    //  $('.button-collapse').sideNav({
+    //   menuWidth: 300, // Default is 240
+    //   edge: 'right', // Choose the horizontal origin
+    //   closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    //   draggable: true // Choose whether you can drag to open on touch screens
+    // });
+        
 
 });
-
-
-
-            // apellido: {
-            //     required: true,
-            //     digits: false,
-            //     minlength: 2
-            //   },
-            // email: {
-            //     required: true,
-            //     digits: true,
-            //     minlength: 2
-            // }
-
-            // apellido: {
-            //     required: "Campo Obligatorio",
-            //     digits: "Ingresa tu Apellido",
-            //     minlength: "Escriba su apellido"
-            //   },
-            // email: {
-            //     required: "El campo es requerido",
-            //     digits: "Ingresa tu E-mail",
-            //     minlength: "Escriba su Email",
-            // }
-
-
-
-
-
-
-
-
 
 
 
