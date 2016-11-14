bb89a8a13b7afb812986438b516b83d6d47f44e4
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-11-14 14:31:34 -0500

    cambios en geo.js

--- js/geo.js
+++ js/geo.js
@@ -40,5 +40,13 @@ $(document).ready(function() {
     //     window.localStorage.setItem("direccion",dir)
     //     content.text(window.localStorage.getItem("direccion"));
     // });
+
+    
+     $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
 });
 
