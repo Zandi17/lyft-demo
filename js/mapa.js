6806cd9310363fea9f8152f75da6e8415b65893c
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-11-14 20:01:38 -0500

    cambios mapa.html

---
+++ js/mapa.js
@@ -0,0 +1,111 @@
+var cargarPagina = function() {
+	if (navigator.geolocation) { 
+		navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+	}
+};
+
+var funcionExito = function(posicion) {
+	var lat = posicion.coords.latitude;
+    var lon = posicion.coords.longitude;
+    var latlon = new google.maps.LatLng(lat, lon);
+    var mapa = document.getElementById('mapa');
+
+    var myOptions = {
+	    center:latlon,zoom:14,
+	    mapTypeId:google.maps.MapTypeId.ROADMAP,
+	    mapTypeControl:false,
+	    navigationControlOptions:{
+	    	style: google.maps.NavigationControlStyle.SMALL
+	   	}
+    };
+    
+    var map = new google.maps.Map(document.getElementById('mapa'), myOptions);
+
+    var marker = new google.maps.Marker({
+    	position:latlon,
+    	map:map,
+    	title:"Tu estas aqui."
+    });
+
+    var dir = "";
+    var latlng = new google.maps.LatLng(lat, lon);
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({"latLng": latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+            dir = results[0].formatted_address;
+        } else {
+            dir = "No se ha podido obtener ninguna dirección en esas coordenadas.";
+        }
+    } 
+    $("#direccion").text(dir);
+    });
+};
+    var funcionbuscar = function(buscar){
+    var input = document.getElementById("inputBuscar");
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); 
+
+    map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+    });
+    }
+
+var funcionError = function (error) {
+	alert("Lo sentimos no encontramos tu ubicación.");
+};
+
+$('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 240
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+});
+
+$(document).ready(cargarPagina);
+
+//   var markers = [];
+//   // Listen for the event fired when the user selects a prediction and retrieve
+//   // more details for that place.
+//   searchBox.addListener('places_changed', function() {
+//     var places = searchBox.getPlaces();
+
+//     if (places.length == 0) {
+//       return;
+//     }
+
+//     // Clear out the old markers.
+//     markers.forEach(function(marker) {
+//       marker.setMap(null);
+//     });
+//     markers = [];
+
+//     // For each place, get the icon, name and location.
+//     var bounds = new google.maps.LatLngBounds();
+//     places.forEach(function(place) {
+//       var icon = {
+//         url: place.icon,
+//         size: new google.maps.Size(71, 71),
+//         origin: new google.maps.Point(0, 0),
+//         anchor: new google.maps.Point(17, 34),
+//         scaledSize: new google.maps.Size(25, 25)
+//       };
+
+//       // Create a marker for each place.
+//       markers.push(new google.maps.Marker({
+//         map: map,
+//         icon: icon,
+//         title: place.name,
+//         position: place.geometry.location
+//       }));
+
+//       if (place.geometry.viewport) {
+//         // Only geocodes have viewport.
+//         bounds.union(place.geometry.viewport);
+//       } else {
+//         bounds.extend(place.geometry.location);
+//       }
+//     });
+//     map.fitBounds(bounds);
+//   });
+// }
